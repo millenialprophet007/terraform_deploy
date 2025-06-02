8d414da90ef14da902b1ec2faedac91b37f66c83
millenialprophet007 <millenialprophet007@gmail.com>
2025-06-02 23:13:52 +0530

    Variables file added

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "content1" {
+  description = "What is the content of File1"
+  default = "Hello world from file one"
+}
+
+variable "content2" {
+  description = "What is the content of File2"
+  default = "Hello world from file two"
+}
