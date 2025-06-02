bace25c63809e87f2b50f22abb9670ba10f45ec5
millenialprophet007 <millenialprophet007@gmail.com>
2025-06-02 23:19:21 +0530

    File names added in variables.tf file

--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "content2" {
   description = "What is the content of File2"
   default = "Hello world from file two"
 }
+
+variable "filename1" {
+  default = "testfile1.txt"
+}
+
+variable "filename2" {
+  default = "testfile2.txt"
+}
