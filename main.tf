6e39515156e0a7b7e4f4049670b493df689e4a9f
millenialprophet007 <millenialprophet007@gmail.com>
2025-06-02 21:35:22 +0530

    file2 created

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "local_file" "file1" {
  content = "hello world"
  filename = "testfile1.txt"
 }
+
+resource "local_file" "file2" {
+ content = "hello world from file2"
+ filename = "testfile2.txt"
+}
